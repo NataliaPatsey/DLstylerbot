64a52bf350b92d7116d53b7a1439aba23a13e512
NataliaPatsey <44480077+NataliaPatsey@users.noreply.github.com>
2022-02-13 15:45:18 +0300

    edit num_step
    set 200

--- MODEL_CONFIG.py
+++ MODEL_CONFIG.py
@@ -5,6 +5,6 @@ cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225])
 content_layers_default = ['conv_4']
 style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 imsize = 256
-num_steps = 100
+num_steps = 200
 style_weight = 1000000
 content_weight = 2
